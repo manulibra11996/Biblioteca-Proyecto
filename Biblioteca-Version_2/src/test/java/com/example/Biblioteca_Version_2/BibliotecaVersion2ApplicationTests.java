9e8c9e31efa18dcf31ca3ab5078c062bfd1387c3
Manuel Fernández Fernández <86914767+manulibra11996@users.noreply.github.com>
2025-06-10 22:27:36 +0200

    Initial commit

---
+++ Biblioteca-Version_2/src/test/java/com/example/Biblioteca_Version_2/BibliotecaVersion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Biblioteca_Version_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaVersion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
