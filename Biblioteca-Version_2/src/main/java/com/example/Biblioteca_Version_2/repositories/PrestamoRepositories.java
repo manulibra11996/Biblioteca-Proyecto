9ebee3f6852a7d4bbac755723b86b3e8d8a272a1
madeandres98 <deandresquintinmiguelangel@gmail.com>
2025-06-11 14:32:17 +0200

    Cambios en las entidades "Libro"

--- Biblioteca-Version_2/src/main/java/com/example/Biblioteca_Version_2/repositories/PrestamoRepositories.java
+++ Biblioteca-Version_2/src/main/java/com/example/Biblioteca_Version_2/repositories/PrestamoRepositories.java
@@ -1,6 +1,5 @@
 package com.example.Biblioteca_Version_2.repositories;
 
-import com.example.Biblioteca_Version_2.entidades.Prestamo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PrestamoRepositories extends JpaRepository<Prestamo, Long> {
